5b3430e8b9f6a49f33a05fe9127258466cab4c8f
pooja <poojamaco55@gmail.com>
2020-10-06 11:21:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div><h1>Anchal Gupta</h1></div>
+     <div>
+        <img src = {'https://www.freepnglogos.com/uploads/google-favicon-logo-20.png'} className="logo" height= {230} width={230} />
+        <img src = {'https://www.freepnglogos.com/uploads/microsoft-window-logo-emblem-0.png'} className="logo" height= {230} width={230} />
+      </div>
+      <div><p>They Both are Competitors</p></div>
+    </div>
+     
+
+  );
+}
+
+export default App;
